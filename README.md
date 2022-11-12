423dbd0338e1c00b6838303671791e1e74761b33
cwillsey06 <scrawlmin@gmail.com>
2022-11-12 00:34:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# neocities-cw
+[![name](https://img.shields.io/github/license/cwillsey06/neocities-cw)](https://github.com/cwillsey06/neocities-cw/blob/main/LICENSE)
+
+My awesome repo for my awesome [neocities](https://neocities.org) website.
---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function main() {
+    # install dependencies
+    # npm i postcss-cli autoprefixer stylus typescript
+
+    # build
+    npx tsc
+    npx stylus src/*.styl --out public/
+    npx postcss public/*.css --use autoprefixer -d public/
+}
+
+time main
